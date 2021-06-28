cdb059f9da2966859538a3aa6eefcafa4be6c62f
Mike Nieva <m@mikenieva.com>
2021-06-28 11:05:13 -0500

    Desarrollo de funcionalidad SALUDO

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("hola mundo")
+
+
+// 1. VARIABLES
+
+const nombre        = document.getElementById("capturar-nombre")
+const btnSaludar    = document.getElementById("btn-saludar")
+const areaSaludo    = document.getElementById("area-saludo")
+
+
+// 2. FUNCIONES
+const saludar = (name) => {
+
+    const saludo = `Hola ${name}!!!`
+
+    return areaSaludo.innerHTML = saludo
+
+}
+
+
+// 3. EVENTOS
+btnSaludar.addEventListener("click", () => {
+
+    const nameValue = nombre.value
+
+    saludar(nameValue)
+    
+})

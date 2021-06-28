b5210a8763651645cf09e7589d26e7f455adc890
Mike Nieva <m@mikenieva.com>
2021-06-28 11:35:15 -0500

    Función de suma terminada

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-console.log("hola mundo")
-
-
 // 1. VARIABLES
 
 const nombre        = document.getElementById("capturar-nombre")
 const btnSaludar    = document.getElementById("btn-saludar")
 const areaSaludo    = document.getElementById("area-saludo")
 
+const valorA        = document.getElementById("valor-a")
+const valorB        = document.getElementById("valor-b")
+const btnSumar      = document.getElementById("btn-sumar")
+const areaSuma      = document.getElementById("area-suma")
 
 // 2. FUNCIONES
 const saludar = (name) => {
 
     const saludo = `Hola ${name}!!!`
-
     return areaSaludo.innerHTML = saludo
 
 }
 
+const sumar = (a, b) => {
+
+    const resultado = a + b
+    return areaSuma.innerHTML = resultado
+
+}
 
 // 3. EVENTOS
 btnSaludar.addEventListener("click", () => {
 
     const nameValue = nombre.value
 
-    saludar(nameValue)
+    return saludar(nameValue)
     
+})
+
+btnSumar.addEventListener("click", () => {
+
+    const inputValorA   = parseInt(valorA.value)
+    const inputValorB   = parseInt(valorB.value)
+
+    return sumar(inputValorA, inputValorB)
+
 })

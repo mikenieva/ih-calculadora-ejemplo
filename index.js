d64dd3ad60e76debedbf9f29d98c6ef0bfd36eb2
Mike Nieva <m@mikenieva.com>
2021-06-28 12:49:06 -0500

    Implementaciónon de valor modular

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import operaciones from './operaciones.js'
+
 // 1. VARIABLES
 
-const nombre        = document.getElementById("capturar-nombre")
+const nombre        = document.querySelector("#capturar-nombre")
 const btnSaludar    = document.getElementById("btn-saludar")
 const areaSaludo    = document.getElementById("area-saludo")
 
@@ -9,33 +11,16 @@ const valorB        = document.getElementById("valor-b")
 const btnSumar      = document.getElementById("btn-sumar")
 const areaSuma      = document.getElementById("area-suma")
 
-// 2. FUNCIONES
-const saludar = (name) => {
-
-    const saludo = `Hola ${name}!!!`
-    return areaSaludo.innerHTML = saludo
-
-}
-
-
-/**
- * DENTRO DE LA PROGRAMACIÓN FUNCIONAL, EXISTE UN CONCEPTO LLAMADO
- * PUREZA.
- * LA PUREZA DE LAS FUNCIONES TE DICE QUE DEBEN SER LO MÁS INDEPENDIENTES POSIBLES. SUS ÚNICOS ACCESOS ESTÁN DENTRO DE LA MISMA FUNCIÓN Y A TRAVÉS DE SUS PARÁMETROS. NO MÁS.
- * */ 
-
-const sumar = (a, b) => {
-    
-    return a + b
-    
-}
 
 // 3. EVENTOS
 btnSaludar.addEventListener("click", () => {
 
     const nameValue = nombre.value
 
-    return saludar(nameValue)
+    const resultado = operaciones.saludar(nameValue)
+
+    areaSaludo.innerHTML = resultado
+
     
 })
 
@@ -44,7 +29,7 @@ btnSumar.addEventListener("click", () => {
     const inputValorA   = parseInt(valorA.value)
     const inputValorB   = parseInt(valorB.value)
 
-    const resultado = sumar(inputValorA, inputValorB)
+    const resultado = operaciones.sumar(inputValorA, inputValorB)
 
     areaSuma.innerHTML = resultado
 
---
+++ operaciones.js
@@ -0,0 +1,11 @@
+const saludar = (name) => {
+
+    return `Hola ${name}!!!`
+
+}
+
+ const sumar = (a, b) => {
+    return a + b    
+    }
+
+export default { saludar, sumar }
